5e3ab20691732f00d4fef40cd95736c75ff6efe6
sammy <samuel.brifo@yahoo.com>
2021-09-15 13:52:37 +0000

    index method implement of redis, admin repo

--- app/repositories/admin_repository.py
+++ app/repositories/admin_repository.py
@@ -22,14 +22,22 @@ class AdminRepository(SQLBaseRepository):
         return result
 
 
-# def index(self):
-#     from_crud = super(AdminRepository,self).index()  # all entries from SQL Based Database saved as list.
-#     cached_data = []  #
-#     if from_crud is [] or None: # recovery options from REDIS FIRST.
-#             redis_key = "admin__"
-#             for i in range(len(from_crud)): # we search for each admin model's id from list of AdminModels
-#                  redis_key = redis_key + from_crud[i].id # in redis, admin key + the id ==> redis insertion key.
-#                  cached_data.append(self.redis_service.get(f"{redis_key}")) # then get from REDIS and append in cached data list.
-#             return cached_data # exit as list of admins -> REDIS Cache database.
-#     # default to CRUD as usual.
-#     return from_crud # exit as a list of admins -> SQL Based Database.
+    def index(self):
+        from_crud = super(AdminRepository,self).index()  # all entries from SQL Based Database saved as list.
+        cached_data = []  #
+        print("outside recovery") # we are outside redis search.
+        if len(from_crud) is 0: # recovery options from REDIS FIRST.
+             count = 1 # count is 0
+             print("inside recovery") # are we inside redis search
+             try:
+                 while True:
+                        redis_key = "admin__" + str(count) # in redis, admin key + the id ==> redis insertion key.
+                        redis_data = self.redis_service.get(f"{redis_key}")
+                        cached_data.append(redis_data) # then get from REDIS and append in cached data list.
+                        count = count + 1 # increase count by one.
+                        if redis_data is None:
+                           return cached_data  # exit as list of admins -> REDIS Cache database
+             except TypeError:
+                 return cached_data
+        # default to CRUD as usual.
+        return from_crud # exit as a list of admins -> SQL Based Database.
